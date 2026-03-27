ded76f093c3e9f11ddb5a90dced19046899c7dbf
drosdick <jim@drosdick.com>
2026-03-26 22:13:28 -0500

    Fix logging entries in Save-ApiKey.ps1

--- StartHere/Save-ApiKey.ps1
+++ StartHere/Save-ApiKey.ps1
@@ -57,8 +57,9 @@ Save-ApiKey -ApiKey $apiKey
 
 Import-Module "C:\ProgramData\Syncro\DrOsdicks\bin\DrModuleV4.psm1" -DisableNameChecking
 
-Initialize-Job "v4 Save-ApiKey" -NoNewTicket
+#Initialize-Job "v4 Save-ApiKey" -NoNewTicket
+Initialize-Job -Subject "V4 Save-ApiKey" -IssueType 'Automation' -InitialIssue "This is the first test." 
 
-Add-LogEntry "API key has been saved." -Icon 'lockandkey' -Buffer $Global:DrLogSummaryBuffer -FlushBuffer
+Add-LogEntry "API key has been saved." -Icon 'lockandkey' -Buffer $Global:DrLogSummaryBuffer
 
-#Add-LogEntry "📤 DrModuleV4 has been downloaded" -Icon 'download' -Buffer $Global:DrLogSummaryBuffer -
+Add-LogEntry "📤 DrModuleV4 has been downloaded" -Icon 'download' -Buffer $Global:DrLogSummaryBuffer -FlushBuffer
